8a0f6150d5b2798143e074c8236957b703b7ff83
Raihan <rhnashil@gmail.com>
2023-04-04 23:00:45 +0700

    ionic

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'manager',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
---
+++ src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyA_3H3e5vHBjl8Txe6E0m9jLaA4hkXzYV4",
+    authDomain: "toko-f70a3.firebaseapp.com",
+    projectId: "toko-f70a3",
+    storageBucket: "toko-f70a3.appspot.com",
+    messagingSenderId: "231480906478",
+    appId: "1:231480906478:web:c96cda615ecad3d05f59ff",
+    measurementId: "G-C1E9YX8LLX"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
